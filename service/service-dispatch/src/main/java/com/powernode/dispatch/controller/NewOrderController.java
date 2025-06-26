7cd0d173b3fd63e57917e67312fc15e424308d4b
pat <pat@bjpowernode>
2025-06-26 10:33:23 +0800

    收到订单信息之后将订单信息发送到xxljob并启动定时任务

--- service/service-dispatch/src/main/java/com/powernode/dispatch/controller/NewOrderController.java
+++ service/service-dispatch/src/main/java/com/powernode/dispatch/controller/NewOrderController.java
@@ -1,7 +1,14 @@
 package com.powernode.dispatch.controller;
 
+import com.powernode.common.result.Result;
+import com.powernode.dispatch.service.NewOrderService;
+import com.powernode.model.vo.dispatch.NewOrderTaskVo;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +19,14 @@ import org.springframework.web.bind.annotation.RestController;
 @SuppressWarnings({"unchecked", "rawtypes"})
 public class NewOrderController {
 
+    @Resource
+    private NewOrderService newOrderService;
 
+    @Operation(summary = "添加新订单派单任务")
+    @PostMapping("/addAndStartTask")
+    public Result<Long> addAndStartTask(@RequestBody NewOrderTaskVo newOrderTaskVo) {
+        return Result.ok(newOrderService.addAndStartTask(newOrderTaskVo));
+    }
 
 }
 
--- service/service-dispatch/src/main/java/com/powernode/dispatch/service/impl/NewOrderServiceImpl.java
+++ service/service-dispatch/src/main/java/com/powernode/dispatch/service/impl/NewOrderServiceImpl.java
@@ -1,9 +1,18 @@
 package com.powernode.dispatch.service.impl;
 
 
+import com.alibaba.fastjson.JSONObject;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.powernode.dispatch.client.XxlJobClient;
+import com.powernode.dispatch.mapper.OrderJobMapper;
 import com.powernode.dispatch.service.NewOrderService;
+import com.powernode.model.entity.dispatch.OrderJob;
+import com.powernode.model.vo.dispatch.NewOrderTaskVo;
+import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 
 @Slf4j
@@ -11,4 +20,35 @@ import org.springframework.stereotype.Service;
 @SuppressWarnings({"unchecked", "rawtypes"})
 public class NewOrderServiceImpl implements NewOrderService {
 
+    @Resource
+    private OrderJobMapper orderJobMapper;
+
+    @Resource
+    private XxlJobClient xxlJobClient;
+
+    @Override
+    public Long addAndStartTask(NewOrderTaskVo newOrderTaskVo) {
+        //根据id查询订单任务
+        LambdaQueryWrapper<OrderJob> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(OrderJob::getOrderId, newOrderTaskVo.getOrderId());
+        OrderJob orderJob = orderJobMapper.selectOne(queryWrapper);
+
+        //若没有则创建
+        if (orderJob == null) {
+            //向xxl-job添加定时任务
+            Long jobId = xxlJobClient.addAndStart("newOrderTaskHandler", "", "0/5 * * * * ?", "新订单任务");
+
+            //向orderJob表中添加数据
+            orderJob = new OrderJob();
+            orderJob.setOrderId(newOrderTaskVo.getOrderId());
+            orderJob.setJobId(jobId);
+            orderJob.setCreateTime(new Date());
+            //设置订单任务参数
+            orderJob.setParameter(JSONObject.toJSONString(newOrderTaskVo));
+
+            orderJobMapper.insert(orderJob);
+
+        }
+        return orderJob.getJobId();
+    }
 }
