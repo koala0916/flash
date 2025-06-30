5141a6726ba254006393ed541f5378a4674972bc
pat <pat@bjpowernode>
2025-06-30 11:13:08 +0800

    分布式锁使用redisson改造抢单多线程环境下的问题

--- service/service-order/src/main/java/com/powernode/order/service/impl/OrderInfoServiceImpl.java
+++ service/service-order/src/main/java/com/powernode/order/service/impl/OrderInfoServiceImpl.java
@@ -14,6 +14,8 @@ import com.powernode.order.mapper.OrderInfoMapper;
 import com.powernode.order.mapper.OrderStatusLogMapper;
 import com.powernode.order.service.OrderInfoService;
 import jakarta.annotation.Resource;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.BeanUtils;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
@@ -36,6 +38,9 @@ public class OrderInfoServiceImpl extends ServiceImpl<OrderInfoMapper, OrderInfo
     @Resource
     private OrderInfoMapper orderInfoMapper;
 
+    @Resource
+    private RedissonClient redissonClient;
+
 
     /**
      * 用户下订单
@@ -53,10 +58,10 @@ public class OrderInfoServiceImpl extends ServiceImpl<OrderInfoMapper, OrderInfo
 
         //设置订单状态  接单状态
         orderInfo.setStatus(OrderStatus.WAITING_ACCEPT.getStatus());
-        
+
         //添加订单
         orderInfoMapper.insert(orderInfo);
-        
+
         //记录日志
         OrderStatusLog orderStatusLog = new OrderStatusLog();
         orderStatusLog.setOrderId(orderInfo.getId());
@@ -67,7 +72,7 @@ public class OrderInfoServiceImpl extends ServiceImpl<OrderInfoMapper, OrderInfo
         orderStatusLogMapper.insert(orderStatusLog);
 
         //将订单状态放入redis中 表示是否被接单
-        redisTemplate.opsForValue().set(RedisConstant.ORDER_ACCEPT_MARK+orderInfo.getId(), orderInfo.getStatus(), RedisConstant.ORDER_ACCEPT_MARK_EXPIRES_TIME, TimeUnit.MINUTES);
+        redisTemplate.opsForValue().set(RedisConstant.ORDER_ACCEPT_MARK + orderInfo.getId(), orderInfo.getStatus(), RedisConstant.ORDER_ACCEPT_MARK_EXPIRES_TIME, TimeUnit.MINUTES);
 
         return orderInfo.getId();
     }
@@ -96,6 +101,7 @@ public class OrderInfoServiceImpl extends ServiceImpl<OrderInfoMapper, OrderInfo
     @Transactional
     @Override
     public Boolean robNewOrder(Long driverId, Long orderId) {
+
         //判断redis中的订单状态是否还存在
         Boolean orderStatus = redisTemplate.hasKey(RedisConstant.ORDER_ACCEPT_MARK + orderId);
         if (!orderStatus) {
@@ -103,29 +109,58 @@ public class OrderInfoServiceImpl extends ServiceImpl<OrderInfoMapper, OrderInfo
             throw new PowerException(ResultCodeEnum.COB_NEW_ORDER_FAIL);
         }
 
-        //抢单
-        OrderInfo orderInfo = new OrderInfo();
-        orderInfo.setId(orderId);
-        orderInfo.setAcceptTime(new Date());//配送员接单时间
-        orderInfo.setStatus(OrderStatus.ACCEPTED.getStatus());//修改订单状态为已接单
-        orderInfo.setDriverId(driverId);//接单的配送员id
 
-        int num = orderInfoMapper.updateById(orderInfo);
-        if (num != 1) {
-            //修改订单失败
-            throw new PowerException(ResultCodeEnum.COB_NEW_ORDER_FAIL);
+        //使用redisson实现分布式锁
+        RLock lock = redissonClient.getLock(RedisConstant.ROB_NEW_ORDER_LOCK + orderId);
+
+        boolean flag = false;
+        try {
+            //尝试获取锁,尝试1秒,若没有获取到锁 则放弃 可以走服务降级 告诉配送员抢单失败
+            flag = lock.tryLock(RedisConstant.ROB_NEW_ORDER_LOCK_WAIT_TIME, RedisConstant.ROB_NEW_ORDER_LOCK_LEASE_TIME, TimeUnit.SECONDS);
+
+            if (flag) {
+                //若程序走到这里，则说明获取锁成功
+                //再次判断redis的订单状态是否存在
+                orderStatus = redisTemplate.hasKey(RedisConstant.ORDER_ACCEPT_MARK + orderId);
+                if (!orderStatus) {
+                    //抢单失败  订单已经被别人抢走了
+                    throw new PowerException(ResultCodeEnum.COB_NEW_ORDER_FAIL);
+                }
+            }
+
+            //抢单  若程序走到这里，则说明有资格可以抢单
+            OrderInfo orderInfo = new OrderInfo();
+            orderInfo.setId(orderId);
+            orderInfo.setAcceptTime(new Date());//配送员接单时间
+            orderInfo.setStatus(OrderStatus.ACCEPTED.getStatus());//修改订单状态为已接单
+            orderInfo.setDriverId(driverId);//接单的配送员id
+
+            int num = orderInfoMapper.updateById(orderInfo);
+            if (num != 1) {
+                //修改订单失败
+                throw new PowerException(ResultCodeEnum.COB_NEW_ORDER_FAIL);
+            }
+
+            //记录日志
+            OrderStatusLog orderStatusLog = new OrderStatusLog();
+            orderStatusLog.setOrderId(orderInfo.getId());
+            orderStatusLog.setOrderStatus(orderInfo.getStatus());
+            orderStatusLog.setOperateTime(new Date());
+            orderStatusLogMapper.insert(orderStatusLog);
+
+
+            //配送员抢单后删除redis中的该订单数据
+            redisTemplate.delete(RedisConstant.ORDER_ACCEPT_MARK + orderInfo.getId());
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }finally {
+            //释放锁  判断锁是否被当前线程持有
+            if (lock.isHeldByCurrentThread() && lock.isLocked()) {
+                lock.unlock();
+            }
+
         }
 
-        //记录日志
-        OrderStatusLog orderStatusLog = new OrderStatusLog();
-        orderStatusLog.setOrderId(orderInfo.getId());
-        orderStatusLog.setOrderStatus(orderInfo.getStatus());
-        orderStatusLog.setOperateTime(new Date());
-        orderStatusLogMapper.insert(orderStatusLog);
-
-
-        //配送员抢单后删除redis中的该订单数据
-        redisTemplate.delete(RedisConstant.ORDER_ACCEPT_MARK + orderInfo.getId());
 
         return true;
     }
